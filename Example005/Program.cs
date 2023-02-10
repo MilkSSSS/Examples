746829d2346c5b77cbf6a3e3ec28976f34c43ff7
Mila <milks0593@gmail.com>
2023-02-10 18:25:32 +0300

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Put your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("URA! It's Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
